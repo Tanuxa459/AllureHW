bebc5ae353f3bbe6bc8a96b3b67828a6f81a9963
Tanya <kliminatatianayog@gmail.com>
2025-01-07 16:38:40 +0300

    Initial commit

--- src/test/java/tests/AllureReportTest.java
+++ src/test/java/tests/AllureReportTest.java
@@ -1,47 +1,40 @@
 package tests;
 
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
-
-
 public class AllureReportTest extends TestBase{
 
-    public static final String REPOSITORY = "Tanuxa459/AllureHW";
-    public static final String TITLE = "Welcome to issues!";
-    @Test
+        public static final String REPOSITORY = "Tanuxa459/AllureHW";
+        public static final String TITLE = "Welcome to issues!";
+        @Test
+        void selenideTest (){
 
-            open("Tanuxa459/AllureHW");
-            $("#issues-tab").click();
-            $(".container-md").shouldHave(text("Welcome to issues!"));
-    }
-    @Test
+                open("Tanuxa459/AllureHW");
+                $("#issues-tab").click();
+                $(".container-md").shouldHave(text("Welcome to issues!"));
+        }
+        @Test
+        void lambdaTest (){
 
-        step("Открываем страницу с репозиторием" + REPOSITORY, () -> {
-            open(REPOSITORY);
-        });
-        step("Нажимаем на вкладку issues", () -> {
-            $("#issues-tab").click();
-        });
-        step("Проверяем строку на кладке issues", () -> {
-            $(".container-md").shouldHave(text(TITLE));
-        });
+                step("Открываем страницу с репозиторием" + REPOSITORY, () -> {
+                        open(REPOSITORY);
+                });
+                step("Нажимаем на вкладку issues", () -> {
+                        $("#issues-tab").click();
+                });
+                step("Проверяем строку на кладке issues", () -> {
+                        $(".container-md").shouldHave(text(TITLE));
+                });
+        }
+        @Test
+        public void annotetedStepsTest(){
 
-    }
-    @Test
-    public void annotetedStepsTest(){
-
-        WebSteps step = new WebSteps();
-        step.openRepository(REPOSITORY);
-        step.clickUssues();
-        step.checkTitle(TITLE);
-
-
-    }
-
+                WebSteps step = new WebSteps();
+                step.openRepository(REPOSITORY);
+                step.clickUssues();
+                step.checkTitle(TITLE);
+        }
 }
-
-
